98842409c0510a149034dd12df5fa8447c7860cb
unknown <unknown@localhost>
2008-02-04 19:55:52 +0000

    Updated comments, made class static.

--- ParserCore/ReferenceValues/Magic.cs
+++ ParserCore/ReferenceValues/Magic.cs
@@ -5,8 +5,16 @@ using System.Text;
 
 namespace WaywardGamers.KParser
 {
-    public class MagicNumbers
+    /// <summary>
+    /// Class to hold any 'special' values that are set due to some
+    /// random determination.
+    /// </summary>
+    public static class MagicNumbers
     {
+        // The minimum time that SQLCE server can handle in a Date/Time field is
+        // January 01, 1753.  Using the standard Windows DateTime.MinValue (January 01, 0001)
+        // generates an overflow error.  Since I'm not sure if the 1753 date accounts
+        // for variations in time zones, I added 1 day to set the magic value.
         public static readonly DateTime MinSQLDateTime = DateTime.Parse("January 02, 1753 0:00 AM");
     }
 }
