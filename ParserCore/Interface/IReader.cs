dbd0f047ae40503556d94b6c1704089a8f16ff12
unknown <unknown@localhost>
2008-02-08 02:24:57 +0000

    Lost interface file.

---
+++ ParserCore/Interface/IReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WaywardGamers.KParser.Interface
+{
+    /// <summary>
+    /// Interface for readers of incoming FFXI information logs.
+    /// </summary>
+    internal interface IReader
+    {
+        void Run();
+        void Stop();
+
+        bool IsRunning { get; }
+    }
+}
