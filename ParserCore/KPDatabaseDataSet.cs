0d6f23fea1e86b41ec6d05ad6d8b38f138062947
unknown <unknown@localhost>
2008-02-12 18:56:36 +0000

    Implemented null/value checking in all database find/update functions.  Commented all functions.

--- ParserCore/KPDatabaseDataSet.cs
+++ ParserCore/KPDatabaseDataSet.cs
@@ -12,12 +12,20 @@ namespace WaywardGamers.KParser
         /// </summary>
         public partial class BattlesDataTable
         {
-
+            /// <summary>
+            /// Get the default battle row.
+            /// </summary>
+            /// <returns>Returns the default battle row, or null if it is not found.</returns>
             public BattlesRow GetDefaultBattle()
             {
                 return this.SingleOrDefault(b => b.DefaultBattle == true);
             }
 
+            /// <summary>
+            /// Gets a list of all battle rows that have not been closed (marked as killed
+            /// or timed out).
+            /// </summary>
+            /// <returns>A list of all currently active battles.</returns>
             public IEnumerable<BattlesRow> GetActiveBattles()
             {
                 var activeBattles = from b in this
@@ -34,6 +42,12 @@ namespace WaywardGamers.KParser
         /// </summary>
         public partial class BattlesRow
         {
+            /// <summary>
+            /// Gets the base experience value for the mob of the given battle.
+            /// This is calculated by removing the experience chain bonus amount
+            /// from the reported value.
+            /// </summary>
+            /// <returns>Returns the base experience value for the mob.</returns>
             public int BaseExperience()
             {
                 if (ExperiencePoints == 0)
@@ -69,16 +83,18 @@ namespace WaywardGamers.KParser
                 return (int)baseXP;
             }
 
+            /// <summary>
+            /// Gets the length of the fight for a given battle.
+            /// </summary>
+            /// <returns>A timespan of the battle's length.</returns>
             public TimeSpan FightLength()
             {
-                if (Killed == true)
+                if ((Killed == true) || (EndTime != MagicNumbers.MinSQLDateTime))
                 {
                     return (EndTime - StartTime);
                 }
-                else
-                {
-                    throw new InvalidOperationException("Cannot get the length of a fight that hasn't ended.");
-                }
+
+                throw new InvalidOperationException("Cannot get the fight length.  No End Time specified.");
             }
         }
 
@@ -87,13 +103,36 @@ namespace WaywardGamers.KParser
         /// </summary>
         public partial class CombatantsDataTable
         {
+            /// <summary>
+            /// Find any combatant with the given name.  Name cannot be null or empty string.
+            /// </summary>
+            /// <param name="name">The name to search for.</param>
+            /// <returns>Returns the table row with that name, or null if the name is not found.</returns>
             public CombatantsRow FindCombatantByName(string name)
             {
+                if (name == null)
+                    throw new ArgumentNullException("name");
+
+                if (name == string.Empty)
+                    throw new ArgumentOutOfRangeException("name", "Name cannot be empty.");
+
                 return this.FirstOrDefault(c => c.CombatantName == name);
             }
 
+            /// <summary>
+            /// Find a player (or unknown) combatant with the given name.
+            /// Name cannot be null or empty string.
+            /// </summary>
+            /// <param name="name">The name to search for.</param>
+            /// <returns>Returns the table row with that name, or null if the name is not found.</returns>
             public CombatantsRow FindPlayerByName(string name)
             {
+                if (name == null)
+                    throw new ArgumentNullException("name");
+
+                if (name == string.Empty)
+                    throw new ArgumentOutOfRangeException("name", "Name cannot be empty.");
+
                 var player = this.SingleOrDefault(c => c.CombatantName == name &&
                     c.CombatantType == (byte)EntityType.Player);
 
@@ -104,9 +143,21 @@ namespace WaywardGamers.KParser
                 return player;
             }
 
+            /// <summary>
+            /// Find a player or pet (or unknown) combatant with the given name.
+            /// Name cannot be null or empty string.
+            /// </summary>
+            /// <param name="name">The name to search for.</param>
+            /// <returns>Returns the table row with that name, or null if the name is not found.</returns>
             public CombatantsRow FindPlayerOrPetByName(string name)
             {
-                var player = this.First(c => c.CombatantName == name &&
+                if (name == null)
+                    throw new ArgumentNullException("name");
+
+                if (name == string.Empty)
+                    throw new ArgumentOutOfRangeException("name", "Name cannot be empty.");
+
+                var player = this.FirstOrDefault(c => c.CombatantName == name &&
                     (c.CombatantType == (byte)EntityType.Player || c.CombatantType == (byte)EntityType.Pet));
 
                 if (player == null)
@@ -116,9 +167,20 @@ namespace WaywardGamers.KParser
                 return player;
             }
 
-
+            /// <summary>
+            /// Find a mob (or unknown) combatant with the given name.
+            /// Name cannot be null or empty string.
+            /// </summary>
+            /// <param name="name">The name to search for.</param>
+            /// <returns>Returns the table row with that name, or null if the name is not found.</returns>
             public CombatantsRow FindMobByName(string name)
             {
+                if (name == null)
+                    throw new ArgumentNullException("name");
+
+                if (name == string.Empty)
+                    throw new ArgumentOutOfRangeException("name", "Name cannot be empty.");
+
                 var mob = this.SingleOrDefault(c => c.CombatantName == name &&
                     c.CombatantType == (byte)EntityType.Mob);
 
@@ -129,8 +191,21 @@ namespace WaywardGamers.KParser
                 return mob;
             }
 
+            /// <summary>
+            /// Find a combatant of specified type by name.
+            /// Name cannot be null or empty string.
+            /// </summary>
+            /// <param name="name">The name to look for.</param>
+            /// <param name="entityType">The entity type to look for.</param>
+            /// <returns>A CombatantsRow containing the specified combatant, or null if not found.</returns>
             public CombatantsRow FindByNameAndType(string name, EntityType entityType)
             {
+                if (name == null)
+                    throw new ArgumentNullException("name");
+
+                if (name == string.Empty)
+                    throw new ArgumentOutOfRangeException("name", "Name cannot be empty.");
+
                 var combatant = this.SingleOrDefault(c => c.CombatantName == name &&
                     c.CombatantType == (byte)entityType);
 
@@ -141,29 +216,42 @@ namespace WaywardGamers.KParser
                 return combatant;
             }
 
+            /// <summary>
+            /// Find a combatant of specified type by name.
+            /// Name cannot be null or empty string.
+            /// Side effects: If the combatant name was found as an Unknown entity type,
+            /// the combatant's type is set to the requested type.
+            /// If combatant was not found, a new row is created and returned.
+            /// </summary>
+            /// <param name="name">The name to look for.</param>
+            /// <param name="entityType">The entity type to look for.</param>
+            /// <returns>A CombatantsRow containing the specified combatant.</returns>
             public CombatantsRow GetCombatant(string name, EntityType entityType)
             {
+                if (name == null)
+                    throw new ArgumentNullException("name");
+
+                if (name == string.Empty)
+                    throw new ArgumentOutOfRangeException("name", "Name cannot be empty.");
+
                 CombatantsRow combatant = null;
 
-                if (name != string.Empty)
+                if (entityType == EntityType.Unknown)
                 {
-                    if (entityType == EntityType.Unknown)
-                    {
-                        combatant = FindCombatantByName(name);
+                    combatant = FindCombatantByName(name);
 
-                        if (combatant == null)
-                            combatant = AddCombatantsRow(name, (byte)entityType, null);
-                    }
-                    else
-                    {
-                        combatant = FindByNameAndType(name, entityType);
+                    if (combatant == null)
+                        combatant = AddCombatantsRow(name, (byte)entityType, null);
+                }
+                else
+                {
+                    combatant = FindByNameAndType(name, entityType);
 
-                        if ((combatant != null) && (combatant.CombatantType != (byte)entityType))
-                            combatant.CombatantType = (byte)entityType;
+                    if ((combatant != null) && (combatant.CombatantType == (byte) EntityType.Unknown))
+                        combatant.CombatantType = (byte)entityType;
 
-                        if (combatant == null)
-                            combatant = AddCombatantsRow(name, (byte)entityType, null);
-                    }
+                    if (combatant == null)
+                        combatant = AddCombatantsRow(name, (byte)entityType, null);
                 }
 
                 return combatant;
@@ -175,23 +263,45 @@ namespace WaywardGamers.KParser
         /// </summary>
         public partial class ItemsDataTable
         {
+            /// <summary>
+            /// Find an item by name.  Name cannot be null or empty string.
+            /// </summary>
+            /// <param name="itemName">The name of the item to look for.</param>
+            /// <returns>The row the item was found on.  Returns null if not found.</returns>
             public ItemsRow FindByItemName(string itemName)
             {
+                if (itemName == null)
+                    throw new ArgumentNullException("itemName");
+
+                if (itemName == string.Empty)
+                    throw new ArgumentOutOfRangeException("itemName", "Name cannot be empty.");
+
                 return this.SingleOrDefault(i => i.ItemName == itemName);
             }
 
+            /// <summary>
+            /// Gets an item with the specified name.
+            /// Name cannot be null or empty string.
+            /// Side effects: If the name is not found in the table, a new
+            /// row is created.
+            /// </summary>
+            /// <param name="itemName">The name to search for.</param>
+            /// <returns>The ItemsRow containing the specified item name.</returns>
             public ItemsRow GetItem(string itemName)
             {
+                if (itemName == null)
+                    throw new ArgumentNullException("itemName");
+
+                if (itemName == string.Empty)
+                    throw new ArgumentOutOfRangeException("itemName", "Name cannot be empty.");
+
                 ItemsRow item = null;
 
-                if (itemName != string.Empty)
-                {
-                    // Get the row for the action name
-                    item = FindByItemName(itemName);
+                // Get the row for the action name
+                item = FindByItemName(itemName);
 
-                    if (item == null)
-                        item = AddItemsRow(itemName);
-                }
+                if (item == null)
+                    item = AddItemsRow(itemName);
 
                 return item;
             }
@@ -202,32 +312,52 @@ namespace WaywardGamers.KParser
         /// </summary>
         public partial class ActionsDataTable
         {
+            /// <summary>
+            /// Find an action by name.  Name cannot be null or empty string.
+            /// </summary>
+            /// <param name="actionName">The name of the action to look for.</param>
+            /// <returns>The row the action was found on.  Returns null if not found.</returns>
             public ActionsRow FindByActionName(string actionName)
             {
-                return this.FirstOrDefault(i => i.ActionName.ToLower() == actionName.ToLower());
+                if (actionName == null)
+                    throw new ArgumentNullException("actionName");
+
+                if (actionName == string.Empty)
+                    throw new ArgumentOutOfRangeException("actionName", "Name cannot be empty.");
+
+                return this.SingleOrDefault(i => i.ActionName.ToLower() == actionName.ToLower());
             }
 
+            /// <summary>
+            /// Gets an action with the specified name.
+            /// Name cannot be null or empty string.
+            /// Side effects: If the name is not found in the table, a new
+            /// row is created.
+            /// </summary>
+            /// <param name="actionName">The name to search for.</param>
+            /// <returns>The ActionsRow containing the specified action name.</returns>
             public ActionsRow GetAction(string actionName)
             {
-                ActionsRow action = null;
+                if (actionName == null)
+                    throw new ArgumentNullException("actionName");
 
-                if ((actionName != null) && (actionName != string.Empty))
+                if (actionName == string.Empty)
+                    throw new ArgumentOutOfRangeException("actionName", "Name cannot be empty.");
+
+                // Get the row for the action name
+                ActionsRow action = FindByActionName(actionName);
+
+                if (action == null)
                 {
-                    // Get the row for the action name
-                    action = FindByActionName(actionName);
-
-                    if (action == null)
+                    // Ensure we don't get fully lower-case entries
+                    if (actionName == actionName.ToLower())
                     {
-                        // Ensure we don't get fully lower-case entries
-                        if (actionName == actionName.ToLower())
-                        {
-                            string firstChar = actionName.Substring(0, 1);
-                            firstChar = firstChar.ToUpper();
-                            actionName = firstChar + actionName.Substring(1);
-                        }
-
-                        action = AddActionsRow(actionName);
+                        string firstChar = actionName.Substring(0, 1);
+                        firstChar = firstChar.ToUpper();
+                        actionName = firstChar + actionName.Substring(1);
                     }
+
+                    action = AddActionsRow(actionName);
                 }
 
                 return action;
@@ -239,23 +369,43 @@ namespace WaywardGamers.KParser
         /// </summary>
         public partial class ChatSpeakersDataTable
         {
+            /// <summary>
+            /// Find a chat speaker by name.  Name cannot be null or empty string.
+            /// </summary>
+            /// <param name="speakerName">The name of the speaker to look for.</param>
+            /// <returns>The row containing the specified speaker.  Returns null if not found.</returns>
             public ChatSpeakersRow FindBySpeakerName(string speakerName)
             {
+                if (speakerName == null)
+                    throw new ArgumentNullException("speakerName");
+
+                if (speakerName == string.Empty)
+                    throw new ArgumentOutOfRangeException("speakerName", "Name cannot be empty.");
+
                 return this.SingleOrDefault(cs => cs.SpeakerName == speakerName);
             }
 
+            /// <summary>
+            /// Gets a chat speaker with the specified name.
+            /// Name cannot be null or empty string.
+            /// Side effects: If the name is not found in the table, a new
+            /// row is created.
+            /// </summary>
+            /// <param name="speakerName">The name of the speaker to look for.</param>
+            /// <returns>The row containing the specified speaker.</returns>
             public ChatSpeakersRow GetSpeaker(string speakerName)
             {
-                ChatSpeakersRow speaker = null;
+                if (speakerName == null)
+                    throw new ArgumentNullException("speakerName");
 
-                if (speakerName != string.Empty)
-                {
-                    // Get the row for the action name
-                    speaker = FindBySpeakerName(speakerName);
+                if (speakerName == string.Empty)
+                    throw new ArgumentOutOfRangeException("speakerName", "Name cannot be empty.");
 
-                    if (speaker == null)
-                        speaker = AddChatSpeakersRow(speakerName);
-                }
+                // Get the row for the action name
+                ChatSpeakersRow speaker = FindBySpeakerName(speakerName);
+
+                if (speaker == null)
+                    speaker = AddChatSpeakersRow(speakerName);
 
                 return speaker;
             }
