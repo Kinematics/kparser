91d8b914e5d800a0b818aade19e622e76438eb0c
unknown <unknown@localhost>
2008-02-14 19:40:18 +0000

    Lock access to Database from property, and only return an instant copy of the database.

--- ParserCore/Database/DatabaseManager.cs
+++ ParserCore/Database/DatabaseManager.cs
@@ -201,7 +201,10 @@ namespace WaywardGamers.KParser
         {
             get
             {
-                return localDB;
+                lock (localDB)
+                {
+                    return localDB.Copy();
+                }
             }
         }
 
