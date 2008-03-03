db9be2d0a68acfd050d44c5a6de89da40c3a44e2
David Smith <dsmith@datasync.com>
2008-03-03 23:42:52 +0000

    Add enhancement/enfeeble plugins. Implement enhancement plugin.

---
+++ PluginMelee/EnfeeblePlugin.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Data;
+using System.Drawing;
+using WaywardGamers.KParser;
+using System.Diagnostics;
+
+namespace WaywardGamers.KParser.Plugin
+{
+    public class EnfeeblePlugin : BasePluginControlWithDropdown
+    {
+        #region IPlugin Overrides
+        public override string TabName
+        {
+            get { return "Debuffs"; }
+        }
+
+        public override void Reset()
+        {
+            richTextBox.Clear();
+            richTextBox.WordWrap = false;
+
+            label1.Text = "Category";
+            comboBox1.Left = label1.Right + 10;
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("All");
+            comboBox1.SelectedIndex = 0;
+
+            label2.Left = comboBox1.Right + 20;
+            label2.Text = "Mob Group";
+            comboBox2.Left = label2.Right + 10;
+            comboBox2.Width = 150;
+            comboBox2.Items.Clear();
+            comboBox2.Items.Add("All");
+            comboBox2.SelectedIndex = 0;
+
+            checkBox1.Enabled = false;
+            checkBox1.Visible = false;
+            checkBox2.Enabled = false;
+            checkBox2.Visible = false;
+        }
+
+        public override void DatabaseOpened(KPDatabaseDataSet dataSet)
+        {
+            ResetComboBox2();
+            AddToComboBox2("All");
+            ResetTextBox();
+
+            if (dataSet.Battles.Count > 1)
+            {
+                var mobsKilled = from b in dataSet.Battles
+                                 where ((b.DefaultBattle == false) &&
+                                        (b.IsEnemyIDNull() == false) &&
+                                        (b.CombatantsRowByEnemyCombatantRelation.CombatantType == (byte)EntityType.Mob))
+                                 orderby b.CombatantsRowByEnemyCombatantRelation.CombatantName
+                                 group b by b.CombatantsRowByEnemyCombatantRelation.CombatantName into bn
+                                 select new
+                                 {
+                                     Name = bn.Key,
+                                     XP = from xb in bn
+                                          group xb by xb.MinBaseExperience() into xbn
+                                          orderby xbn.Key
+                                          select new { BaseXP = xbn.Key }
+                                 };
+
+                if (mobsKilled.Count() > 0)
+                {
+                    // Add to the Reset list
+
+                    string mobWithXP;
+
+                    foreach (var mob in mobsKilled)
+                    {
+                        AddToComboBox2(mob.Name);
+
+                        if (mob.XP.Count() > 1)
+                        {
+                            foreach (var xp in mob.XP)
+                            {
+                                mobWithXP = string.Format("{0} ({1})", mob.Name, xp.BaseXP);
+
+                                AddToComboBox2(mobWithXP);
+
+                                // Check for existing entry with higher min base xp
+                                mobWithXP = string.Format("{0} ({1})", mob.Name, xp.BaseXP + 1);
+
+                                if (comboBox2.Items.Contains(mobWithXP))
+                                    RemoveFromComboBox2(mobWithXP);
+                            }
+                        }
+                    }
+                }
+            }
+
+            InitComboBox2Selection();
+        }
+
+        protected override bool FilterOnDatabaseChanging(DatabaseWatchEventArgs e, out KPDatabaseDataSet datasetToUse)
+        {
+            // Check for new mobs being fought.  If any exist, update the Mob Group dropdown list.
+            if (e.DatasetChanges.Battles.Count > 0)
+            {
+                var mobsFought = from b in e.DatasetChanges.Battles
+                                 where ((b.DefaultBattle == false) &&
+                                        (b.IsEnemyIDNull() == false) &&
+                                        (b.CombatantsRowByEnemyCombatantRelation.CombatantType == (byte)EntityType.Mob))
+                                 group b by b.CombatantsRowByEnemyCombatantRelation.CombatantName into bn
+                                 select new
+                                 {
+                                     Name = bn.Key,
+                                     XP = from xb in bn
+                                          group xb by xb.MinBaseExperience() into xbn
+                                          orderby xbn.Key
+                                          select new { BaseXP = xbn.Key }
+                                 };
+
+
+                if (mobsFought.Count() > 0)
+                {
+                    string mobWithXP;
+
+                    foreach (var mob in mobsFought)
+                    {
+                        if (comboBox2.Items.Contains(mob.Name) == false)
+                            AddToComboBox2(mob.Name);
+
+                        foreach (var xp in mob.XP)
+                        {
+                            if (xp.BaseXP > 0)
+                            {
+                                mobWithXP = string.Format("{0} ({1})", mob.Name, xp.BaseXP);
+
+                                if (comboBox2.Items.Contains(mobWithXP) == false)
+                                    AddToComboBox2(mobWithXP);
+
+                                // Check for existing entry with higher min base xp
+                                mobWithXP = string.Format("{0} ({1})", mob.Name, xp.BaseXP + 1);
+
+                                if (comboBox2.Items.Contains(mobWithXP))
+                                    RemoveFromComboBox2(mobWithXP);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (e.DatasetChanges.Interactions.Count != 0)
+            {
+                var enfeebles = from i in e.DatasetChanges.Interactions
+                                where i.HarmType == (byte)HarmType.Enfeeble
+                                select i;
+
+                if (enfeebles.Count() > 0)
+                {
+                    datasetToUse = e.Dataset;
+                    return true;
+                }
+            }
+
+            datasetToUse = null;
+            return false;
+        }
+        #endregion
+
+        #region Processing sections
+        protected override void ProcessData(KPDatabaseDataSet dataSet)
+        {
+        }
+        #endregion
+
+    }
+}
---
+++ PluginMelee/EnhancePlugin.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Data;
+using System.Drawing;
+using WaywardGamers.KParser;
+using System.Diagnostics;
+
+namespace WaywardGamers.KParser.Plugin
+{
+    public class EnhancePlugin : BasePluginControlWithDropdown
+    {
+        #region IPlugin Overrides
+        public override string TabName
+        {
+            get { return "Buffs"; }
+        }
+
+        public override void Reset()
+        {
+            richTextBox.Clear();
+            richTextBox.WordWrap = false;
+
+            label1.Text = "Category";
+            comboBox1.Left = label1.Right + 10;
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("Buffs Used");
+            comboBox1.Items.Add("Buffs Received");
+            comboBox1.SelectedIndex = 0;
+
+            label2.Enabled = false;
+            label2.Visible = false;
+            comboBox2.Enabled = false;
+            comboBox2.Visible = false;
+
+            checkBox1.Enabled = false;
+            checkBox1.Visible = false;
+            checkBox2.Enabled = false;
+            checkBox2.Visible = false;
+        }
+
+        protected override bool FilterOnDatabaseChanging(DatabaseWatchEventArgs e, out KPDatabaseDataSet datasetToUse)
+        {
+            if (e.DatasetChanges.Interactions.Count != 0)
+            {
+                var enhancements = from i in e.DatasetChanges.Interactions
+                                   where i.AidType == (byte)AidType.Enhance
+                                   select i;
+
+                if (enhancements.Count() > 0)
+                {
+                    datasetToUse = e.Dataset;
+                    return true;
+                }
+            }
+
+            datasetToUse = null;
+            return false;
+        }
+        #endregion
+
+        #region Member Variables
+        string buffUsedHeader = "Buff                Used on             # Times   Min Interval   Max Interval   Avg Interval\n";
+        string buffRecHeader = "Buff                Used by             # Times   Min Interval   Max Interval   Avg Interval\n";
+        #endregion
+
+        #region Processing sections
+        protected override void ProcessData(KPDatabaseDataSet dataSet)
+        {
+            richTextBox.Clear();
+
+            if (comboBox1.SelectedIndex == 0)
+                ProcessBuffsUsed(dataSet);
+            else
+                ProcessBuffsReceived(dataSet);
+
+        }
+
+        private void ProcessBuffsUsed(KPDatabaseDataSet dataSet)
+        {
+            var buffs = from c in dataSet.Combatants
+                        where ((c.CombatantType == (byte)EntityType.Player) ||
+                              (c.CombatantType == (byte)EntityType.Pet) ||
+                              (c.CombatantType == (byte)EntityType.Fellow))
+                        orderby c.CombatantType, c.CombatantName
+                        select new
+                        {
+                            Name = c.CombatantName,
+                            Buffs = from b in c.GetInteractionsRowsByActorCombatantRelation()
+                                    where b.AidType == (byte)AidType.Enhance &&
+                                          b.Preparing == false
+                                    group b by b.ActionsRow.ActionName into ba
+                                    orderby ba.Key
+                                    select new
+                                    {
+                                        BuffName = ba.Key,
+                                        BuffTargets = from bt in ba
+                                                      where bt.IsTargetIDNull() == false
+                                                      group bt by bt.CombatantsRowByTargetCombatantRelation.CombatantName into btn
+                                                      orderby btn.Key
+                                                      select new
+                                                      {
+                                                          TargetName = btn.Key,
+                                                          Buffs = btn.OrderBy(i => i.Timestamp)
+                                                      }
+                                    }
+                        };
+
+
+
+            int used;
+            List<TimeSpan> intervalList = new List<TimeSpan>();
+            TimeSpan minInterval;
+            TimeSpan maxInterval;
+            TimeSpan avgInterval;
+            //double percActive;
+
+            string buffName;
+
+            //StringBuilder sb = new StringBuilder();
+
+            foreach (var player in buffs)
+            {
+                if ((player.Buffs == null) || (player.Buffs.Count() == 0))
+                    continue;
+
+                AppendBoldText(string.Format("{0}\n", player.Name), Color.Blue);
+                AppendBoldUnderText(buffUsedHeader, Color.Black);
+
+                foreach (var buff in player.Buffs)
+                {
+                    buffName = buff.BuffName;
+
+                    foreach (var target in buff.BuffTargets)
+                    {
+                        AppendNormalText(buffName.PadRight(20));
+                        buffName = "";
+
+                        used = target.Buffs.Count();
+                        AppendNormalText(target.TargetName.PadRight(20));
+                        AppendNormalText(used.ToString().PadLeft(7));
+
+                        if (used > 1)
+                        {
+                            avgInterval = TimeSpan.FromSeconds(
+                                (target.Buffs.Last().Timestamp - target.Buffs.First().Timestamp).TotalSeconds / (used - 1));
+
+                            intervalList.Clear();
+
+                            for (int i = 1; i < used; i++)
+                            {
+                                var curr = target.Buffs.ElementAt(i);
+                                var last = target.Buffs.ElementAt(i - 1);
+
+                                intervalList.Add(curr.Timestamp - last.Timestamp);
+                            }
+
+                            minInterval = intervalList.Min();
+                            maxInterval = intervalList.Max();
+
+                            AppendNormalText(string.Format("{0,15}{1,15}{2,15}",
+                                TimespanString(minInterval), TimespanString(maxInterval), TimespanString(avgInterval)));
+                        }
+
+
+                        AppendNormalText("\n");
+                    }
+                }
+
+                AppendNormalText("\n");
+            }
+
+        }
+
+        private void ProcessBuffsReceived(KPDatabaseDataSet dataSet)
+        {
+            var buffs = from c in dataSet.Combatants
+                        where ((c.CombatantType == (byte)EntityType.Player) ||
+                              (c.CombatantType == (byte)EntityType.Pet) ||
+                              (c.CombatantType == (byte)EntityType.Fellow))
+                        orderby c.CombatantType, c.CombatantName
+                        select new
+                        {
+                            Name = c.CombatantName,
+                            Buffs = from b in c.GetInteractionsRowsByTargetCombatantRelation()
+                                    where b.AidType == (byte)AidType.Enhance &&
+                                          b.Preparing == false
+                                    group b by b.ActionsRow.ActionName into ba
+                                    orderby ba.Key
+                                    select new
+                                    {
+                                        BuffName = ba.Key,
+                                        BuffCasters = from bt in ba
+                                                      where bt.IsActorIDNull() == false
+                                                      group bt by bt.CombatantsRowByActorCombatantRelation.CombatantName into btn
+                                                      orderby btn.Key
+                                                      select new
+                                                      {
+                                                          CasterName = btn.Key,
+                                                          Buffs = btn.OrderBy(i => i.Timestamp)
+                                                      }
+                                    }
+                        };
+
+
+
+            int used;
+            List<TimeSpan> intervalList = new List<TimeSpan>();
+            TimeSpan minInterval;
+            TimeSpan maxInterval;
+            TimeSpan avgInterval;
+            //double percActive;
+
+            string buffName;
+
+            //StringBuilder sb = new StringBuilder();
+
+            foreach (var player in buffs)
+            {
+                if ((player.Buffs == null) || (player.Buffs.Count() == 0))
+                    continue;
+
+                AppendBoldText(string.Format("{0}\n", player.Name), Color.Blue);
+                AppendBoldUnderText(buffRecHeader, Color.Black);
+
+                foreach (var buff in player.Buffs)
+                {
+                    buffName = buff.BuffName;
+
+                    foreach (var target in buff.BuffCasters)
+                    {
+                        AppendNormalText(buffName.PadRight(20));
+                        buffName = "";
+
+                        used = target.Buffs.Count();
+                        AppendNormalText(target.CasterName.PadRight(20));
+                        AppendNormalText(used.ToString().PadLeft(7));
+
+                        if (used > 1)
+                        {
+                            avgInterval = TimeSpan.FromSeconds(
+                                (target.Buffs.Last().Timestamp - target.Buffs.First().Timestamp).TotalSeconds / (used - 1));
+
+                            intervalList.Clear();
+
+                            for (int i = 1; i < used; i++)
+                            {
+                                var curr = target.Buffs.ElementAt(i);
+                                var last = target.Buffs.ElementAt(i - 1);
+
+                                intervalList.Add(curr.Timestamp - last.Timestamp);
+                            }
+
+                            minInterval = intervalList.Min();
+                            maxInterval = intervalList.Max();
+
+                            AppendNormalText(string.Format("{0,15}{1,15}{2,15}",
+                                TimespanString(minInterval), TimespanString(maxInterval), TimespanString(avgInterval)));
+                        }
+
+
+                        AppendNormalText("\n");
+                    }
+                }
+
+                AppendNormalText("\n");
+            }
+
+        }
+
+        private string TimespanString(TimeSpan timeSpan)
+        {
+            string tsBlock;
+
+            if (timeSpan.Hours > 0)
+                tsBlock = string.Format("{0}:{1:d2}:{2:d2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+            else
+                tsBlock = string.Format("{0}:{1:d2}", timeSpan.Minutes, timeSpan.Seconds);
+
+
+            return tsBlock;
+        }
+        #endregion
+
+        #region Event Handlers
+        protected override void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            HandleDataset(DatabaseManager.Instance.Database);
+        }
+        #endregion
+    }
+}
