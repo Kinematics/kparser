758216bc804e43ca9fc9686efe6ac9ae8d72ba8f
David Smith <dsmith@datasync.com>
2008-03-07 09:13:56 +0000

    Add process access rights enums.  Not in use yet.

--- ParserCore/Monitors/RamReader/PInvoke.cs
+++ ParserCore/Monitors/RamReader/PInvoke.cs
@@ -59,6 +59,22 @@ namespace WaywardGamers.KParser.Monitoring.Memory
         Execute = 0x0020
     }
 
+    internal enum ProcessAccessRights : uint
+    {
+        Terminate                = 0x0001,
+        CreateThread             = 0x0002,
+        VMOperation              = 0x0008,
+        VMRead                   = 0x0010,
+        VMWrite                  = 0x0020,
+        DuplicateHandle          = 0x0040,
+        CreateProcess            = 0x0080,
+        SetQuota                 = 0x0100,
+        SetInformation           = 0x0200,
+        QueryInformation         = 0x0400,
+        SuspendResume            = 0x0800,
+        QueryLimitedInformation  = 0x1000,
+        Synchronize              = 0x00100000,
+    }
     #endregion
 
     #region Win32 Structures
