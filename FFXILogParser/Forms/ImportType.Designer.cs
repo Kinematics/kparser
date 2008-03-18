fc6d0f318fa8a4e91bf7da01fafda5b3633ef2b3
David Smith <dsmith@datasync.com>
2008-03-18 07:29:11 +0000

    Add form for selecting type of import (only 1 type at present).

---
+++ FFXILogParser/Forms/ImportType.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WaywardGamers.KParser.Forms
+{
+    partial class ImportType
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ok = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.optionDVSParse = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ok
+            // 
+            this.ok.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ok.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.ok.Location = new System.Drawing.Point(76, 80);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(75, 23);
+            this.ok.TabIndex = 0;
+            this.ok.Text = "OK";
+            this.ok.UseVisualStyleBackColor = true;
+            // 
+            // cancel
+            // 
+            this.cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancel.Location = new System.Drawing.Point(157, 80);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 1;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            // 
+            // optionDVSParse
+            // 
+            this.optionDVSParse.AutoSize = true;
+            this.optionDVSParse.Location = new System.Drawing.Point(19, 19);
+            this.optionDVSParse.Name = "optionDVSParse";
+            this.optionDVSParse.Size = new System.Drawing.Size(146, 17);
+            this.optionDVSParse.TabIndex = 2;
+            this.optionDVSParse.TabStop = true;
+            this.optionDVSParse.Text = "DVS Parse / Direct Parse";
+            this.optionDVSParse.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.optionDVSParse);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 53);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Type of database to import";
+            // 
+            // ImportType
+            // 
+            this.AcceptButton = this.ok;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancel;
+            this.ClientSize = new System.Drawing.Size(244, 115);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.ok);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ImportType";
+            this.ShowInTaskbar = false;
+            this.Text = "Import";
+            this.Load += new System.EventHandler(this.ImportType_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.RadioButton optionDVSParse;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ FFXILogParser/Forms/ImportType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WaywardGamers.KParser.Forms
+{
+    public partial class ImportType : Form
+    {
+        public ImportType()
+        {
+            InitializeComponent();
+        }
+
+        private void ImportType_Load(object sender, EventArgs e)
+        {
+            optionDVSParse.Checked = true;
+        }
+
+        internal ImportSource ImportSource
+        {
+            get
+            {
+                if (optionDVSParse.Checked == true)
+                    return ImportSource.DVSParse;
+
+                return ImportSource.Unknown;
+            }
+        }
+    }
+}
