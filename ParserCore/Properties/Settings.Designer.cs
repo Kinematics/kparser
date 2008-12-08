cbb07533b54dfcec4968ea814374bdfeb5988a7a
David Smith <dsmith@datasync.com>
2008-12-08 20:51:33 +0000

    Update RAM base address info. Update changelog.

--- ParserCore/Properties/Settings.Designer.cs
+++ ParserCore/Properties/Settings.Designer.cs
@@ -56,7 +56,7 @@ namespace WaywardGamers.KParser.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("5729624")]
+        [global::System.Configuration.DefaultSettingValueAttribute("5743304")]
         public uint MemoryOffset {
             get {
                 return ((uint)(this["MemoryOffset"]));
