ce7649d8c281896bff953ab2f236a7e79d93b728
David Smith <dsmith@datasync.com>
2008-12-08 19:54:06 +0000

    Lock plugin during HandleDataset function to prevent multiple re-entry deadlock. Ensure that no plugins ever call ProcessData function directly.  All must call through HandleDataset. Tweak HandleDataset function to keep cursor position/selection in place across updates.

--- PluginMelee/DPMPlugin.cs
+++ PluginMelee/DPMPlugin.cs
@@ -73,7 +73,7 @@ namespace WaywardGamers.KParser.Plugin
             flagNoUpdate = true;
             playersCombo.CBSelectIndex(0);
 
-            ProcessData(dataSet);
+            HandleDataset(dataSet);
         }
 
         protected override bool FilterOnDatabaseChanging(DatabaseWatchEventArgs e, out KPDatabaseDataSet datasetToUse)
