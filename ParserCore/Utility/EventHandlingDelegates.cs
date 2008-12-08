59d9315b9d0648f2674b3310c52b878c46a7798c
David Smith <dsmith@datasync.com>
2008-12-08 16:59:30 +0000

    File for keeping event-handling delegates and such together.

---
+++ ParserCore/Utility/EventHandlingDelegates.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+#region Event classes
+public delegate void DatabaseReparseEventHandler(object sender, DatabaseReparseEventArgs ramArgs);
+
+public class DatabaseReparseEventArgs : EventArgs
+{
+    public int RowsRead { get; private set; }
+    public int TotalRows { get; private set; }
+    public bool Complete { get; private set; }
+
+    internal DatabaseReparseEventArgs(int rowRead, int totalRows, bool complete)
+    {
+        RowsRead = rowRead;
+        TotalRows = totalRows;
+        Complete = complete;
+    }
+}
+#endregion
