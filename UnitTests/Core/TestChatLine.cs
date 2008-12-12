f3793aa1fb3781013f544987166d8b9c1446d26d
David Smith <dsmith@datasync.com>
2008-12-12 01:27:52 +0000

    Update comments.  Completed unit test for the ChatLine class.

--- UnitTests/Core/TestChatLine.cs
+++ UnitTests/Core/TestChatLine.cs
@@ -9,6 +9,10 @@ namespace WaywardGamers.KParser
     [TestFixture]
     public class TestChatLine
     {
+        /// <summary>
+        /// Test basic constructor that only takes the chat text.  Verify that the
+        /// assigned timestamp falls between the two DateTime.Now checks.
+        /// </summary>
         [Test]
         public void TestConstruction1()
         {
@@ -21,6 +25,10 @@ namespace WaywardGamers.KParser
             Assert.That(testChatLine.ChatText, Is.EqualTo("Test Line"));
         }
 
+        /// <summary>
+        /// Test the full constructor that includes setting the timestamp.
+        /// Verify that both the text and the timestamps are properly set.
+        /// </summary>
         [Test]
         public void TestConstruction2()
         {
@@ -32,18 +40,27 @@ namespace WaywardGamers.KParser
             Assert.That(testChatLine.ChatText, Is.EqualTo("Test Line"));
         }
 
+        /// <summary>
+        /// Test whether setting the timestamp property works properly.
+        /// </summary>
         [Test]
         public void TestTimestampSet()
         {
             ChatLine testChatLine = new ChatLine("Test Line");
 
-            DateTime timestamp = DateTime.Now;
+            DateTime timestamp = DateTime.Now.AddMinutes(1);
 
             testChatLine.Timestamp = timestamp;
 
             Assert.That(testChatLine.Timestamp, Is.EqualTo(timestamp));
         }
 
+        /// <summary>
+        /// Test the scenario of setting an invalid timestamp (one that falls
+        /// prior to the arbitrarily set MinSQLDateTime value, as determined by
+        /// MS SQL documentation).  In the case of a time that lands too early,
+        /// it needs to set and return the MinSQLDateTime value instead.
+        /// </summary>
         [Test]
         public void TestTimestampNull()
         {
