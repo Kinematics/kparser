98aca3a81af9dd4efc9108a14553862097ab13cb
David Smith <dsmith@datasync.com>
2009-02-16 17:56:19 +0000

    Create new generic event handlers/delegates for the reader classes to use to notify watching entities of new data or progress updates.

--- ParserCore/Utility/EventHandlingDelegates.cs
+++ ParserCore/Utility/EventHandlingDelegates.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace WaywardGamers.KParser
@@ -20,6 +21,38 @@ namespace WaywardGamers.KParser
         }
     }
     #endregion
+
+    #region IReader delegates
+    public delegate void ReaderDataHandler(object sender, ReaderDataEventArgs readerStatusArgs);
+
+    public class ReaderDataEventArgs : EventArgs
+    {
+        public List<ChatLine> ChatLines { get; private set; }
+
+        internal ReaderDataEventArgs(List<ChatLine> chatLines)
+        {
+            ChatLines = chatLines;
+        }
+    }
+
+    public delegate void ReaderStatusHandler(object sender, ReaderStatusEventArgs readerStatusArgs);
+
+    public class ReaderStatusEventArgs : EventArgs
+    {
+        public int Completed { get; private set; }
+        public int Total { get; private set; }
+        public bool Complete { get; private set; }
+
+        internal ReaderStatusEventArgs(int completed, int total, bool complete)
+        {
+            Completed = completed;
+            Total = total;
+            Complete = complete;
+        }
+    }
+
+    #endregion
+
 
     #region Watching the database
     public delegate void DatabaseWatchEventHandler(object sender, DatabaseWatchEventArgs dbArgs);
