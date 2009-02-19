12ebb2a41fabc587f32bbb856d977f92f4800b5b
David Smith <dsmith@datasync.com>
2009-02-19 02:42:48 +0000

    Adjusted namespace reference.

--- UnitTests/Core/TestMessageLine.cs
+++ UnitTests/Core/TestMessageLine.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using WaywardGamers.KParser.Monitoring;
 
 namespace WaywardGamers.KParser.Messages
 {
