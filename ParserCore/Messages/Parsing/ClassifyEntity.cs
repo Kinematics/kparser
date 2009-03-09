b0e89235d8b0306085652ac132db9ce659ec1e12
David Smith <dsmith@datasync.com>
2009-03-09 04:40:54 +0000

    Rewrite entire classification schema for resolving uncertain entity types.

--- ParserCore/Messages/Parsing/ClassifyEntity.cs
+++ ParserCore/Messages/Parsing/ClassifyEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace WaywardGamers.KParser.Parsing
@@ -23,13 +24,8 @@ namespace WaywardGamers.KParser.Parsing
                 return EntityType.Unknown;
             }
 
-            // Check if we already have this name in the entity list.  If so, use that.
-            EntityType entityType = EntityManager.Instance.LookupEntity(entityName);
-            if (entityType != EntityType.Unknown)
-                return entityType;
-
             // If the entityName starts with 'the' it's a mob, though possibly a charmed one.
-            if (entityName.StartsWith("The ") || entityName.StartsWith("the "))
+            if (entityName.StartsWith("The ", StringComparison.InvariantCultureIgnoreCase))
             {
                 return EntityType.Mob;
             }
@@ -88,76 +84,87 @@ namespace WaywardGamers.KParser.Parsing
             return EntityType.Player;
         }
 
-
-        internal static void DetermineCharmedPet(ref CombatDetails combatDetails, ref TargetDetails target, bool death)
+        internal static EntityType ClassifyByNameAndLookup(string entityName)
         {
-            if (combatDetails == null)
-                throw new ArgumentNullException("combatDetails");
-
-            if (target == null)
-                throw new ArgumentNullException("target");
-
-            // Handle identifying charmed pet entities
-            if (target.EntityType == EntityType.Mob && combatDetails.ActorEntityType == EntityType.Mob)
+            if ((entityName == null) || (entityName == string.Empty))
             {
-                EntityType checkTarg = EntityManager.Instance.LookupPetEntity(target.Name);
-                EntityType checkActor = EntityManager.Instance.LookupPetEntity(combatDetails.ActorName);
-
-                if ((checkActor == EntityType.Pet) ^ (checkTarg == EntityType.Pet))
-                {
-                    // If only one shows up as being a pet, use that.
-                    if (checkActor == EntityType.Pet)
-                        combatDetails.ActorEntityType = checkActor;
-                    else
-                        target.EntityType = checkTarg;
-                }
-                else if ((checkActor == EntityType.Pet) && (checkTarg == EntityType.Pet))
-                {
-                    // If both show up as being a pet, check the last charmed mob
-                    // to break the tie.
-                    string lastPetName = EntityManager.Instance.LastAddedPetEntity;
-
-                    if (lastPetName != string.Empty)
-                    {
-                        // Check to make sure we don't have dhalmel fighting dhalmel, or whatever.
-                        if (target.Name != combatDetails.ActorName)
-                        {
-                            if (target.Name == lastPetName)
-                            {
-                                target.EntityType = EntityType.Pet;
-                            }
-                            else if (combatDetails.ActorName == lastPetName)
-                            {
-                                combatDetails.ActorEntityType = EntityType.Pet;
-                            }
-                        }
-                        else
-                        {
-                            // actor and target are same mob type; cannot accurately determine
-                            // which is the mob and which is the pet; random?
-                            if (death == true)
-                            {
-                                combatDetails.FlagPetDeath = true;
-                            }
-                            else
-                            {
-                                if (random.Next(1000) < 500)
-                                {
-                                    target.EntityType = EntityType.Pet;
-                                }
-                                else
-                                {
-                                    combatDetails.ActorEntityType = EntityType.Pet;
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        // if no last pet entry, ignore
-                    }
-                }
+                return EntityType.Unknown;
             }
+
+            // Check if we already have this name in the entity list.  If so, use that.
+            List<EntityType> entityTypeList = EntityManager.Instance.LookupEntity(entityName);
+
+            if (entityTypeList.Count == 1)
+            {
+                if (entityTypeList[0] != EntityType.Unknown)
+                    return entityTypeList[0];
+            }
+
+            // If there are multiple entries in the entity manager's list (ie: normal + charmed),
+            // we cannot determine the entity type by name alone.
+            if (entityTypeList.Count > 1)
+            {
+                return EntityType.Unknown;
+            }
+
+            // If the entityName starts with 'the' it's a mob, though possibly a charmed one.
+            if (entityName.StartsWith("The ", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return EntityType.Mob;
+            }
+
+            // If the entityName starts with 'SC: ', it's a skillchain.  Mark it as such.
+            if (entityName.StartsWith("SC: "))
+                return EntityType.Skillchain;
+
+            // Check for characters that only show up in mob names and some sublists.
+            Match entityNameMatch = ParseExpressions.MobNameTest.Match(entityName);
+
+            if (entityNameMatch.Success == true)
+            {
+                // Probably a mob, but possibly a pet.  Check the short names lists.
+                if (Puppets.ShortNamesList.Contains(entityName))
+                    return EntityType.Pet;
+                else if (Wyverns.ShortNamesList.Contains(entityName))
+                    return EntityType.Pet;
+                else if (NPCFellows.ShortNamesList.Contains(entityName))
+                    return EntityType.Fellow;
+                else
+                    return EntityType.Mob;
+            }
+
+            // Check for the pattern of beastmaster jug pet entityNames.
+            entityNameMatch = ParseExpressions.BstJugPetName.Match(entityName);
+            if (entityNameMatch.Success == true)
+            {
+                return EntityType.Pet;
+            }
+
+            // Check known pet lists
+            if (Avatars.NamesList.Contains(entityName))
+            {
+                return EntityType.Pet;
+            }
+
+            if (Wyverns.NamesList.Contains(entityName))
+            {
+                return EntityType.Pet;
+            }
+
+            if (Puppets.NamesList.Contains(entityName))
+            {
+                return EntityType.Pet;
+            }
+
+            // Check known NPC fellows
+            if (NPCFellows.NamesList.Contains(entityName))
+            {
+                return EntityType.Fellow;
+            }
+
+
+            // Anything else must be a player.
+            return EntityType.Player;
         }
 
         internal static void VerifyEntities(ref Message message, ref TargetDetails target, bool death)
@@ -174,36 +181,152 @@ namespace WaywardGamers.KParser.Parsing
             uint msgCode = message.MessageCode;
             CombatDetails combatDetails = message.EventDetails.CombatDetails;
 
-            // If both entities are players, verify this isn't a combat message
-            // where one should be a mob.  If it is, the mob is probably a
-            // misclassified Notorius Monster.  Adjust as needed.
-            if (combatDetails.ActorEntityType == EntityType.Player &&
-                target.EntityType == EntityType.Player)
-            {
-                EntityType checkActor = ParseCodes.Instance.GetActorEntityType(msgCode);
-                if (checkActor == EntityType.Mob)
-                {
-                    combatDetails.ActorEntityType = EntityType.Mob;
-                    EntityManager.Instance.OverridePlayerToMob(combatDetails.ActorName);
-                    return;
-                }
 
-                EntityType checkTarget = ParseCodes.Instance.GetTargetEntityType(msgCode);
-                if (checkTarget == EntityType.Mob)
+
+            // If the message is of type Aid, both entities should be of the same type.
+
+            if (combatDetails.InteractionType == InteractionType.Aid)
+            {
+                // If one or the other (but not both) are type mob, one of them
+                // must be charmed.  Since I don't believe it's possible for a mob
+                // to aid a charmed player, and a charmed player cannot do any actions
+                // except melee, if one of these is a mob, it must be charmed.
+                if ((combatDetails.ActorEntityType == EntityType.Mob) ^
+                    (target.EntityType == EntityType.Mob))
                 {
-                    target.EntityType = EntityType.Mob;
-                    EntityManager.Instance.OverridePlayerToMob(target.Name);
-                    return;
+                    // However exclude instances of a skillchain as an actor in
+                    // cases of skillchains healing mobs (eg: elementals, puks, etc).
+                    if (combatDetails.ActorEntityType != EntityType.Skillchain)
+                    {
+                        if (combatDetails.ActorEntityType == EntityType.Mob)
+                            combatDetails.ActorEntityType = EntityType.Charmed;
+                        else if (target.EntityType == EntityType.Mob)
+                            target.EntityType = EntityType.Charmed;
+                    }
                 }
 
                 return;
             }
 
-            // If both entities are mobs, run the check for charmed pets.
-            if (combatDetails.ActorEntityType == EntityType.Mob &&
-                target.EntityType == EntityType.Mob)
+            // If the message is of type Harm, the entities should be of different types.
+
+            if (combatDetails.InteractionType == InteractionType.Harm)
             {
-                DetermineCharmedPet(ref combatDetails, ref target, death);
+                // If both entities are mobs, one of them must be charmed.
+                if ((combatDetails.ActorEntityType == EntityType.Mob) &&
+                    (target.EntityType == EntityType.Mob))
+                {
+                    // If they have different names, see if we have one registered
+                    // as the most recently charmed mob.
+                    if (combatDetails.ActorName != target.Name)
+                    {
+                        if (combatDetails.ActorName == EntityManager.Instance.LastCharmedMob)
+                        {
+                            combatDetails.ActorEntityType = EntityType.Charmed;
+                            return;
+                        }
+
+                        if (target.Name == EntityManager.Instance.LastCharmedMob)
+                        {
+                            target.EntityType = EntityType.Charmed;
+                            return;
+                        }
+
+                        // If neither are registered as the most recent, see if either
+                        // (but not both) have been registered as charmed.
+                        EntityType actorCharmEntity = EntityManager.Instance.LookupCharmedEntity(combatDetails.ActorName);
+                        EntityType targetCharmEntity = EntityManager.Instance.LookupCharmedEntity(target.Name);
+
+                        if ((actorCharmEntity == EntityType.Charmed) ^ (targetCharmEntity == EntityType.Charmed))
+                        {
+                            if (actorCharmEntity == EntityType.Charmed)
+                            {
+                                combatDetails.ActorEntityType = EntityType.Charmed;
+                                return;
+                            }
+
+                            if (targetCharmEntity == EntityType.Charmed)
+                            {
+                                target.EntityType = EntityType.Charmed;
+                                return;
+                            }
+                        }
+
+                    }
+
+                    // At this point, either the mobs have the same name, or both mobs are
+                    // listed in our entity list as possibly charmed and neither one is our
+                    // 'last charmed' mob.  Cannot accurately determine which is which, so random.
+
+                    if (death == true)
+                    {
+                        // If it's a death, defer resolution until we know if we
+                        // get experience.
+                        combatDetails.FlagPetDeath = true;
+                    }
+                    else
+                    {
+                        // Otherwise just random it.
+                        if (random.Next(1000) < 500)
+                            target.EntityType = EntityType.Charmed;
+                        else
+                            combatDetails.ActorEntityType = EntityType.Charmed;
+                    }
+                }
+
+                // If both entities are players, one of them must be charmed,
+                // or one of them is an NM.
+                // Charmed players will not attack NPCs/Pets/etc.
+                if ((combatDetails.ActorEntityType == EntityType.Player) &&
+                    (target.EntityType == EntityType.Player))
+                {
+                    // Charmed players can only melee.  If this is anything aside from
+                    // melee, one of the entities must be an NM
+                    if (combatDetails.ActionType != ActionType.Melee)
+                    {
+                        // This is probably an NM.  Set the entity type based on the message code.
+                        if (ParseCodes.Instance.GetActorEntityType(msgCode) == EntityType.Mob)
+                        {
+                            combatDetails.ActorEntityType = EntityType.Mob;
+                            return;
+                        }
+                        else if (ParseCodes.Instance.GetTargetEntityType(msgCode) == EntityType.Mob)
+                        {
+                            target.EntityType = EntityType.Mob;
+                            return;
+                        }
+                    }
+
+                    // If we get here, this is either melee, or we're not sure of the
+                    // defined entity types.  Try looking up details from the entity manager first.
+                    EntityType actorCharmEntity = EntityManager.Instance.LookupCharmedEntity(combatDetails.ActorName);
+                    if (actorCharmEntity == EntityType.Charmed)
+                    {
+                        combatDetails.ActorEntityType = EntityType.Charmed;
+                        return;
+                    }
+
+                    EntityType targetCharmEntity = EntityManager.Instance.LookupCharmedEntity(target.Name);
+                    if (targetCharmEntity == EntityType.Charmed)
+                    {
+                        target.EntityType = EntityType.Charmed;
+                        return;
+                    }
+
+                    // If we don't get anything from the entity manager, assume the
+                    // aggressive melee'er is the charmed one.
+                    combatDetails.ActorEntityType = EntityType.Charmed;
+                    return;
+                }
+
+                // If a pet is attacking a player, that player must be charmed.
+                if ((combatDetails.ActorEntityType == EntityType.Pet) &&
+                    (target.EntityType == EntityType.Player))
+                {
+                    target.EntityType = EntityType.Charmed;
+                    return;
+                }
+
             }
         }
     }
