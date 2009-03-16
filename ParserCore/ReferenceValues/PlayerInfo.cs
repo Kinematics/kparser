f428d8dc1b1849abd7ebdc629f4dc44b3060053c
David Smith <dsmith@datasync.com>
2009-03-16 19:30:27 +0000

    Add class to store player info passed between the DatabaseReader and the DatabaseManager.

---
+++ ParserCore/ReferenceValues/PlayerInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WaywardGamers.KParser
+{
+    internal class PlayerInfo
+    {
+        public string Name { get; set; }
+        public EntityType CombatantType { get; set; }
+        public string Info { get; set; }
+    }
+}
