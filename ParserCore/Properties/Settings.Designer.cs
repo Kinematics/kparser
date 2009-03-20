19fa6b3e50feb786060acd431f61c95dbaaf052c
David Smith <dsmith@datasync.com>
2009-03-20 23:50:07 +0000

    Update settings files with new UpgradeRequired property. On program load, use this as a flag to indicate whether to Upgrade the settings, which will carry over previous version settings values. Update of version numbers (done early).

--- ParserCore/Properties/Settings.Designer.cs
+++ ParserCore/Properties/Settings.Designer.cs
@@ -171,5 +171,17 @@ namespace WaywardGamers.KParser.Properties {
                 this["DefaultParseSaveDirectory"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool UpgradeRequired {
+            get {
+                return ((bool)(this["UpgradeRequired"]));
+            }
+            set {
+                this["UpgradeRequired"] = value;
+            }
+        }
     }
 }
