76276a64f1f93877f979886a5ad2d5a921bcb7a4
David Smith <dsmith@datasync.com>
2009-08-16 21:50:05 +0000

    Renamed all resource files to not use 'plugin' as part of the name.  Resources are local to the dll, not to the plugins themselves.

--- PluginMelee/Resources/Combat.Designer.cs
+++ PluginMelee/Resources/Combat.Designer.cs
@@ -22,14 +22,14 @@ namespace WaywardGamers.KParser.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "2.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class CombatPlugin {
+    internal class Combat {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal CombatPlugin() {
+        internal Combat() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace WaywardGamers.KParser.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WaywardGamers.KParser.Resources.CombatPlugin", typeof(CombatPlugin).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WaywardGamers.KParser.Resources.Combat", typeof(Combat).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
