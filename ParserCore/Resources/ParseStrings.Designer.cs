016038678fec031927175c9ff9a64646633e9ae2
David Smith <dsmith@datasync.com>
2009-08-17 04:41:21 +0000

    Make sure ParseStrings resource strings are publicly accessible.

--- ParserCore/Resources/ParseStrings.Designer.cs
+++ ParserCore/Resources/ParseStrings.Designer.cs
@@ -22,7 +22,7 @@ namespace WaywardGamers.KParser.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "2.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class ParseStrings {
+    public class ParseStrings {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace WaywardGamers.KParser.Resources {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WaywardGamers.KParser.Resources.ParseStrings", typeof(ParseStrings).Assembly);
@@ -51,7 +51,7 @@ namespace WaywardGamers.KParser.Resources {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace WaywardGamers.KParser.Resources {
         /// <summary>
         ///   Looks up a localized string similar to Gil.
         /// </summary>
-        internal static string Gil {
+        public static string Gil {
             get {
                 return ResourceManager.GetString("Gil", resourceCulture);
             }
