986e4cac064922d73735891dee2e59dc1b7d320a
David Smith <dsmith@datasync.com>
2009-08-18 19:28:49 +0000

    Rename resources file names to make clear that these are for already-parsed data, not for use in actually parsing the game.

--- ParserCore/Resources/ParsedStrings.Designer.cs
+++ ParserCore/Resources/ParsedStrings.Designer.cs
@@ -22,14 +22,14 @@ namespace WaywardGamers.KParser.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "2.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class ParseStrings {
+    public class ParsedStrings {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal ParseStrings() {
+        internal ParsedStrings() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace WaywardGamers.KParser.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WaywardGamers.KParser.Resources.ParseStrings", typeof(ParseStrings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WaywardGamers.KParser.Resources.ParsedStrings", typeof(ParsedStrings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
