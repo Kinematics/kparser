ad94c7e5b0895702f0a67d4937cfd2d46d8db50d
David Smith <dsmith@datasync.com>
2009-09-01 02:43:55 +0000

    Set resources for process selection dialog.

--- ParserCore/Monitors/RamReader/SelectPOLProcess.Designer.cs
+++ ParserCore/Monitors/RamReader/SelectPOLProcess.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SelectPOLProcess));
             this.processList = new System.Windows.Forms.ListBox();
             this.ok = new System.Windows.Forms.Button();
             this.cancel = new System.Windows.Forms.Button();
@@ -36,68 +37,69 @@
             // 
             // processList
             // 
-            this.processList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.processList.AccessibleDescription = null;
+            this.processList.AccessibleName = null;
+            resources.ApplyResources(this.processList, "processList");
+            this.processList.BackgroundImage = null;
+            this.processList.Font = null;
             this.processList.FormattingEnabled = true;
-            this.processList.Location = new System.Drawing.Point(12, 15);
             this.processList.Name = "processList";
-            this.processList.Size = new System.Drawing.Size(268, 82);
-            this.processList.TabIndex = 0;
             this.processList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.processList_MouseDoubleClick);
             // 
             // ok
             // 
-            this.ok.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ok.AccessibleDescription = null;
+            this.ok.AccessibleName = null;
+            resources.ApplyResources(this.ok, "ok");
+            this.ok.BackgroundImage = null;
             this.ok.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.ok.Location = new System.Drawing.Point(124, 116);
+            this.ok.Font = null;
             this.ok.Name = "ok";
-            this.ok.Size = new System.Drawing.Size(75, 23);
-            this.ok.TabIndex = 1;
-            this.ok.Text = "OK";
             this.ok.UseVisualStyleBackColor = true;
             this.ok.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ok_MouseClick);
             // 
             // cancel
             // 
-            this.cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancel.AccessibleDescription = null;
+            this.cancel.AccessibleName = null;
+            resources.ApplyResources(this.cancel, "cancel");
+            this.cancel.BackgroundImage = null;
             this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancel.Location = new System.Drawing.Point(205, 116);
+            this.cancel.Font = null;
             this.cancel.Name = "cancel";
-            this.cancel.Size = new System.Drawing.Size(75, 23);
-            this.cancel.TabIndex = 2;
-            this.cancel.Text = "Cancel";
             this.cancel.UseVisualStyleBackColor = true;
             this.cancel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.cancel_MouseClick);
             // 
             // refresh
             // 
-            this.refresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.refresh.Location = new System.Drawing.Point(12, 116);
+            this.refresh.AccessibleDescription = null;
+            this.refresh.AccessibleName = null;
+            resources.ApplyResources(this.refresh, "refresh");
+            this.refresh.BackgroundImage = null;
+            this.refresh.Font = null;
             this.refresh.Name = "refresh";
-            this.refresh.Size = new System.Drawing.Size(75, 23);
-            this.refresh.TabIndex = 3;
-            this.refresh.Text = "Refresh";
             this.refresh.UseVisualStyleBackColor = true;
             this.refresh.Click += new System.EventHandler(this.refresh_Click);
             // 
             // SelectPOLProcess
             // 
             this.AcceptButton = this.ok;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AccessibleDescription = null;
+            this.AccessibleName = null;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = null;
             this.CancelButton = this.cancel;
-            this.ClientSize = new System.Drawing.Size(292, 151);
             this.Controls.Add(this.refresh);
             this.Controls.Add(this.cancel);
             this.Controls.Add(this.ok);
             this.Controls.Add(this.processList);
+            this.Icon = null;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SelectPOLProcess";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
-            this.Text = "Select POL Process";
             this.Load += new System.EventHandler(this.SelectPOLProcess_Load);
             this.ResumeLayout(false);
 
