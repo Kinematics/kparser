29c929c9aa6e648368b63438025b5ee7f45d433c
David Smith <dsmith@datasync.com>
2009-11-30 10:39:45 +0000

    Rearrange layout of LinqExtensions file.  Add original version of the grouping extension for future reference.

--- ParserCore/Utility/LinqExtensions.cs
+++ ParserCore/Utility/LinqExtensions.cs
@@ -9,7 +9,7 @@ namespace WaywardGamers.KParser
     /// Static class for adding general utility extension functions
     /// to IEnumerable classes.
     /// </summary>
-    static class LinqExtensions
+    public static class LinqExtensions
     {
         /// <summary>
         /// Gets the maximum element of an IEnumerable source based on a provided comparison
@@ -91,66 +91,63 @@ namespace WaywardGamers.KParser
 
             return maxElement;
         }
-    }
 
-
-
-    public static class StringExtensions
-    {
-        public static string FormattedString(this TimeSpan timeSpan, bool forceIncludeHours)
+        /// <summary>
+        /// The generalized grouping extension method.  It groups lists of adjacently
+        /// identical elements together.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <returns></returns>
+        public static IEnumerable<IGrouping<TKey, TSource>> GroupAdjacent<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
         {
-            if (timeSpan == null)
-                return default(string);
+            TKey last = default(TKey);
+            bool haveLast = false;
+            List<TSource> list = new List<TSource>();
 
-            string formattedTimeSpan = string.Empty;
-
-            double baseSeconds = timeSpan.Seconds + ((double)timeSpan.Milliseconds / 1000);
-            int roundedSeconds = (int)Math.Round(baseSeconds);
-
-            if ((forceIncludeHours == true) || (timeSpan.Hours > 0))
+            foreach (TSource s in source)
             {
-                formattedTimeSpan = string.Format("{0:d}:{1,2:d2}:{2,2:d2}",
-                    (int)timeSpan.TotalHours, timeSpan.Minutes,
-                    roundedSeconds);
+                TKey k = keySelector(s);
+                if (haveLast)
+                {
+                    if (!k.Equals(last))
+                    {
+                        yield return new GroupOfAdjacent<TSource, TKey>(list, last);
+                        list = new List<TSource>();
+                        list.Add(s);
+                        last = k;
+                    }
+                    else
+                    {
+                        list.Add(s);
+                        last = k;
+                    }
+                }
+                else
+                {
+                    list.Add(s);
+                    last = k;
+                    haveLast = true;
+                }
             }
-            else
-            {
-                formattedTimeSpan = string.Format("{0:d}:{1,2:d2}",
-                    (int)timeSpan.TotalMinutes,
-                    roundedSeconds);
-            }
-
-
-            return formattedTimeSpan;
-        }
-    }
-
-
-    public class GroupOfAdjacent<TSource, TKey> : IEnumerable<TSource>, IGrouping<TKey, TSource>
-    {
-        public TKey Key { get; set; }
-        private List<TSource> GroupList { get; set; }
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-        {
-            return ((System.Collections.Generic.IEnumerable<TSource>)this).GetEnumerator();
+            if (haveLast)
+                yield return new GroupOfAdjacent<TSource, TKey>(list, last);
         }
 
-        System.Collections.Generic.IEnumerator<TSource> System.Collections.Generic.IEnumerable<TSource>.GetEnumerator()
-        {
-            foreach (var s in GroupList)
-                yield return s;
-        }
-
-        public GroupOfAdjacent(List<TSource> source, TKey key)
-        {
-            GroupList = source;
-            Key = key;
-        }
-    }
-
-    public static class LinqTimeExtensions
-    {
+        /// <summary>
+        /// A specialized version of the grouping extension that groups adjacent elements
+        /// that are within a spefified time limit of each other together.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <param name="adjacentTime"></param>
+        /// <returns></returns>
         public static IEnumerable<IGrouping<TKey, TSource>> GroupAdjacentByTimeLimit<TSource, TKey>(
             this IEnumerable<TSource> source,
             Func<TSource, DateTime> keySelector,
@@ -189,8 +186,70 @@ namespace WaywardGamers.KParser
                 }
             }
             if (haveLast)
-                yield return (IGrouping<TKey, TSource>)(new GroupOfAdjacent<TSource, DateTime>(list, last));
+                yield return (IGrouping<TKey, TSource>)(new GroupOfAdjacent<TSource, DateTime>(list, first));
         }
     }
 
+
+    /// <summary>
+    /// A class to hold extensions specifically designed to generate specially formatted strings.
+    /// </summary>
+    public static class StringExtensions
+    {
+        public static string FormattedShortTimeString(this TimeSpan timeSpan, bool forceIncludeHours)
+        {
+            if (timeSpan == null)
+                return default(string);
+
+            string formattedTimeSpan = string.Empty;
+
+            double baseSeconds = timeSpan.Seconds + ((double)timeSpan.Milliseconds / 1000);
+            int roundedSeconds = (int)Math.Round(baseSeconds);
+
+            if ((forceIncludeHours == true) || (timeSpan.Hours > 0))
+            {
+                formattedTimeSpan = string.Format("{0:d}:{1,2:d2}:{2,2:d2}",
+                    (int)timeSpan.TotalHours, timeSpan.Minutes,
+                    roundedSeconds);
+            }
+            else
+            {
+                formattedTimeSpan = string.Format("{0:d}:{1,2:d2}",
+                    (int)timeSpan.TotalMinutes,
+                    roundedSeconds);
+            }
+
+
+            return formattedTimeSpan;
+        }
+    }
+
+
+    /// <summary>
+    /// A grouping enumeration class for use by the grouping extensions.
+    /// </summary>
+    /// <typeparam name="TSource">The object to group.</typeparam>
+    /// <typeparam name="TKey">The key to group on.</typeparam>
+    public class GroupOfAdjacent<TSource, TKey> : IEnumerable<TSource>, IGrouping<TKey, TSource>
+    {
+        public TKey Key { get; set; }
+        private List<TSource> GroupList { get; set; }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return ((System.Collections.Generic.IEnumerable<TSource>)this).GetEnumerator();
+        }
+
+        System.Collections.Generic.IEnumerator<TSource> System.Collections.Generic.IEnumerable<TSource>.GetEnumerator()
+        {
+            foreach (var s in GroupList)
+                yield return s;
+        }
+
+        public GroupOfAdjacent(List<TSource> source, TKey key)
+        {
+            GroupList = source;
+            Key = key;
+        }
+    }
 }
