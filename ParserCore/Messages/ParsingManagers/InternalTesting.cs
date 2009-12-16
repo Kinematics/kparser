79c80077a77e1ed156d095776831344b8c5dd37c
David Smith <dsmith@datasync.com>
2009-12-16 04:29:47 +0000

    Revise test run.

--- ParserCore/Messages/ParsingManagers/InternalTesting.cs
+++ ParserCore/Messages/ParsingManagers/InternalTesting.cs
@@ -10,16 +10,18 @@ namespace WaywardGamers.KParser.Parsing
         public void RunATest()
         {
 
-            string chatText1 = "3b,ee,d0,80808050,000011db,000014f1,0022,00,01,02,00,The Qiqirn Astrologer uses Faze.";
-            string chatText2 = "3b,ee,d0,80808050,000011db,000014f2,0019,00,01,02,00,No effect on Motenten.1";
+            string chatText1 = "3b,18,2d,80808050,00001ef4,000023b8,001d,00,01,02,00,Motenten uses Spectral Jig.";
+            string chatText2 = "3b,18,2d,80808050,00001ef4,000023b9,0019,00,01,02,00,No effect on Motenten.1";
             ChatLine chatLine1 = new ChatLine(chatText1);
             MessageLine msgLine1 = new MessageLine(chatLine1);
             ChatLine chatLine2 = new ChatLine(chatText2);
             MessageLine msgLine2 = new MessageLine(chatLine2);
 
+            MsgManager.Instance.Reset();
+
             Message msg1 = Parser.Parse(msgLine1);
 
-            MsgManager.Instance.Reset();
+            //MsgManager.Instance.Reset();
             EntityManager.Instance.AddEntitiesFromMessage(msg1);
             MsgManager.Instance.AddMessageToMessageCollection(msg1);
 
