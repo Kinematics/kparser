d5d0edc7957265296515163678ec4a3e57ed3e5e
David Smith <dsmith@datasync.com>
2010-01-15 22:52:09 +0000

    Update French interface text with corrections provided in issue 49. Base resource file auto-regenerated.

--- FFXILogParser/Forms/Options.Designer.cs
+++ FFXILogParser/Forms/Options.Designer.cs
@@ -58,7 +58,10 @@ namespace WaywardGamers.KParser
             // 
             // dataSourceGroup
             // 
+            this.dataSourceGroup.AccessibleDescription = null;
+            this.dataSourceGroup.AccessibleName = null;
             resources.ApplyResources(this.dataSourceGroup, "dataSourceGroup");
+            this.dataSourceGroup.BackgroundImage = null;
             this.dataSourceGroup.Controls.Add(this.specifyPID);
             this.dataSourceGroup.Controls.Add(this.editMemoryAddress);
             this.dataSourceGroup.Controls.Add(this.readExistingLogs);
@@ -69,68 +72,107 @@ namespace WaywardGamers.KParser
             this.dataSourceGroup.Controls.Add(this.getLogDirectory);
             this.dataSourceGroup.Controls.Add(this.dataSourceRam);
             this.dataSourceGroup.Controls.Add(this.dataSourceLogs);
+            this.dataSourceGroup.Font = null;
             this.dataSourceGroup.Name = "dataSourceGroup";
             this.dataSourceGroup.TabStop = false;
             // 
             // specifyPID
             // 
+            this.specifyPID.AccessibleDescription = null;
+            this.specifyPID.AccessibleName = null;
             resources.ApplyResources(this.specifyPID, "specifyPID");
+            this.specifyPID.BackgroundImage = null;
+            this.specifyPID.Font = null;
             this.specifyPID.Name = "specifyPID";
             this.specifyPID.UseVisualStyleBackColor = true;
             // 
             // editMemoryAddress
             // 
+            this.editMemoryAddress.AccessibleDescription = null;
+            this.editMemoryAddress.AccessibleName = null;
             resources.ApplyResources(this.editMemoryAddress, "editMemoryAddress");
+            this.editMemoryAddress.BackgroundImage = null;
+            this.editMemoryAddress.Font = null;
             this.editMemoryAddress.Name = "editMemoryAddress";
             this.editMemoryAddress.UseVisualStyleBackColor = true;
             this.editMemoryAddress.CheckedChanged += new System.EventHandler(this.editMemoryAddress_CheckedChanged);
             // 
             // readExistingLogs
             // 
+            this.readExistingLogs.AccessibleDescription = null;
+            this.readExistingLogs.AccessibleName = null;
             resources.ApplyResources(this.readExistingLogs, "readExistingLogs");
+            this.readExistingLogs.BackgroundImage = null;
+            this.readExistingLogs.Font = null;
             this.readExistingLogs.Name = "readExistingLogs";
             this.readExistingLogs.UseVisualStyleBackColor = true;
             // 
             // memoryOffsetAddress
             // 
+            this.memoryOffsetAddress.AccessibleDescription = null;
+            this.memoryOffsetAddress.AccessibleName = null;
             resources.ApplyResources(this.memoryOffsetAddress, "memoryOffsetAddress");
+            this.memoryOffsetAddress.BackgroundImage = null;
+            this.memoryOffsetAddress.Font = null;
             this.memoryOffsetAddress.Name = "memoryOffsetAddress";
             this.memoryOffsetAddress.ReadOnly = true;
             this.memoryOffsetAddress.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.memoryOffsetAddress_KeyPress);
             // 
             // logDirectory
             // 
+            this.logDirectory.AccessibleDescription = null;
+            this.logDirectory.AccessibleName = null;
             resources.ApplyResources(this.logDirectory, "logDirectory");
+            this.logDirectory.BackgroundImage = null;
+            this.logDirectory.Font = null;
             this.logDirectory.Name = "logDirectory";
             this.logDirectory.ReadOnly = true;
             // 
             // memoryLabel
             // 
+            this.memoryLabel.AccessibleDescription = null;
+            this.memoryLabel.AccessibleName = null;
             resources.ApplyResources(this.memoryLabel, "memoryLabel");
+            this.memoryLabel.Font = null;
             this.memoryLabel.Name = "memoryLabel";
             // 
             // directoryLabel
             // 
+            this.directoryLabel.AccessibleDescription = null;
+            this.directoryLabel.AccessibleName = null;
             resources.ApplyResources(this.directoryLabel, "directoryLabel");
+            this.directoryLabel.Font = null;
             this.directoryLabel.Name = "directoryLabel";
             // 
             // getLogDirectory
             // 
+            this.getLogDirectory.AccessibleDescription = null;
+            this.getLogDirectory.AccessibleName = null;
             resources.ApplyResources(this.getLogDirectory, "getLogDirectory");
+            this.getLogDirectory.BackgroundImage = null;
+            this.getLogDirectory.Font = null;
             this.getLogDirectory.Name = "getLogDirectory";
             this.getLogDirectory.UseVisualStyleBackColor = true;
             this.getLogDirectory.Click += new System.EventHandler(this.getLogDirectory_Click);
             // 
             // dataSourceRam
             // 
+            this.dataSourceRam.AccessibleDescription = null;
+            this.dataSourceRam.AccessibleName = null;
             resources.ApplyResources(this.dataSourceRam, "dataSourceRam");
+            this.dataSourceRam.BackgroundImage = null;
+            this.dataSourceRam.Font = null;
             this.dataSourceRam.Name = "dataSourceRam";
             this.dataSourceRam.TabStop = true;
             this.dataSourceRam.UseVisualStyleBackColor = true;
             // 
             // dataSourceLogs
             // 
+            this.dataSourceLogs.AccessibleDescription = null;
+            this.dataSourceLogs.AccessibleName = null;
             resources.ApplyResources(this.dataSourceLogs, "dataSourceLogs");
+            this.dataSourceLogs.BackgroundImage = null;
+            this.dataSourceLogs.Font = null;
             this.dataSourceLogs.Name = "dataSourceLogs";
             this.dataSourceLogs.TabStop = true;
             this.dataSourceLogs.UseVisualStyleBackColor = true;
@@ -138,53 +180,83 @@ namespace WaywardGamers.KParser
             // 
             // ok
             // 
+            this.ok.AccessibleDescription = null;
+            this.ok.AccessibleName = null;
             resources.ApplyResources(this.ok, "ok");
+            this.ok.BackgroundImage = null;
             this.ok.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.ok.Font = null;
             this.ok.Name = "ok";
             this.ok.UseVisualStyleBackColor = true;
             this.ok.Click += new System.EventHandler(this.ok_Click);
             // 
             // cancel
             // 
+            this.cancel.AccessibleDescription = null;
+            this.cancel.AccessibleName = null;
             resources.ApplyResources(this.cancel, "cancel");
+            this.cancel.BackgroundImage = null;
             this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancel.Font = null;
             this.cancel.Name = "cancel";
             this.cancel.UseVisualStyleBackColor = true;
             this.cancel.Click += new System.EventHandler(this.cancel_Click);
             // 
             // reset
             // 
+            this.reset.AccessibleDescription = null;
+            this.reset.AccessibleName = null;
             resources.ApplyResources(this.reset, "reset");
+            this.reset.BackgroundImage = null;
+            this.reset.Font = null;
             this.reset.Name = "reset";
             this.reset.UseVisualStyleBackColor = true;
             this.reset.Click += new System.EventHandler(this.reset_Click);
             // 
             // debugMode
             // 
+            this.debugMode.AccessibleDescription = null;
+            this.debugMode.AccessibleName = null;
             resources.ApplyResources(this.debugMode, "debugMode");
+            this.debugMode.BackgroundImage = null;
+            this.debugMode.Font = null;
             this.debugMode.Name = "debugMode";
             this.debugMode.UseVisualStyleBackColor = true;
             // 
+            // folderBrowserDialog
+            // 
+            resources.ApplyResources(this.folderBrowserDialog, "folderBrowserDialog");
+            // 
             // otherGroup
             // 
+            this.otherGroup.AccessibleDescription = null;
+            this.otherGroup.AccessibleName = null;
             resources.ApplyResources(this.otherGroup, "otherGroup");
+            this.otherGroup.BackgroundImage = null;
             this.otherGroup.Controls.Add(this.label1);
             this.otherGroup.Controls.Add(this.numberOfRecentFilesUpDown);
             this.otherGroup.Controls.Add(this.defaultSaveDirectory);
             this.otherGroup.Controls.Add(this.saveDirectoryLabel);
             this.otherGroup.Controls.Add(this.getSaveDirectory);
             this.otherGroup.Controls.Add(this.debugMode);
+            this.otherGroup.Font = null;
             this.otherGroup.Name = "otherGroup";
             this.otherGroup.TabStop = false;
             // 
             // label1
             // 
+            this.label1.AccessibleDescription = null;
+            this.label1.AccessibleName = null;
             resources.ApplyResources(this.label1, "label1");
+            this.label1.Font = null;
             this.label1.Name = "label1";
             // 
             // numberOfRecentFilesUpDown
             // 
+            this.numberOfRecentFilesUpDown.AccessibleDescription = null;
+            this.numberOfRecentFilesUpDown.AccessibleName = null;
             resources.ApplyResources(this.numberOfRecentFilesUpDown, "numberOfRecentFilesUpDown");
+            this.numberOfRecentFilesUpDown.Font = null;
             this.numberOfRecentFilesUpDown.Maximum = new decimal(new int[] {
             10,
             0,
@@ -204,18 +276,29 @@ namespace WaywardGamers.KParser
             // 
             // defaultSaveDirectory
             // 
+            this.defaultSaveDirectory.AccessibleDescription = null;
+            this.defaultSaveDirectory.AccessibleName = null;
             resources.ApplyResources(this.defaultSaveDirectory, "defaultSaveDirectory");
+            this.defaultSaveDirectory.BackgroundImage = null;
+            this.defaultSaveDirectory.Font = null;
             this.defaultSaveDirectory.Name = "defaultSaveDirectory";
             this.defaultSaveDirectory.ReadOnly = true;
             // 
             // saveDirectoryLabel
             // 
+            this.saveDirectoryLabel.AccessibleDescription = null;
+            this.saveDirectoryLabel.AccessibleName = null;
             resources.ApplyResources(this.saveDirectoryLabel, "saveDirectoryLabel");
+            this.saveDirectoryLabel.Font = null;
             this.saveDirectoryLabel.Name = "saveDirectoryLabel";
             // 
             // getSaveDirectory
             // 
+            this.getSaveDirectory.AccessibleDescription = null;
+            this.getSaveDirectory.AccessibleName = null;
             resources.ApplyResources(this.getSaveDirectory, "getSaveDirectory");
+            this.getSaveDirectory.BackgroundImage = null;
+            this.getSaveDirectory.Font = null;
             this.getSaveDirectory.Name = "getSaveDirectory";
             this.getSaveDirectory.UseVisualStyleBackColor = true;
             this.getSaveDirectory.Click += new System.EventHandler(this.getSaveDirectory_Click);
@@ -223,15 +306,20 @@ namespace WaywardGamers.KParser
             // Options
             // 
             this.AcceptButton = this.ok;
+            this.AccessibleDescription = null;
+            this.AccessibleName = null;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = null;
             this.CancelButton = this.cancel;
             this.Controls.Add(this.otherGroup);
             this.Controls.Add(this.reset);
             this.Controls.Add(this.cancel);
             this.Controls.Add(this.ok);
             this.Controls.Add(this.dataSourceGroup);
+            this.Font = null;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = null;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Options";
