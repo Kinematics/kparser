319fc4495934e1506ce28a03e06794e6551e9f4b
David Smith <dsmith@datasync.com>
2010-03-20 23:49:18 +0000

    Add Join to the IReader interface.

--- ParserCore/Interface/IReader.cs
+++ ParserCore/Interface/IReader.cs
@@ -11,6 +11,7 @@ namespace WaywardGamers.KParser.Interface
         void Import(ImportSourceType importSource, IDBReader dbReaderManager, bool modifyTimestamp);
         void ImportRange(ImportSourceType importSource, IDBReader dbReaderManager, bool modifyTimestamp,
             DateTime startOfRange, DateTime endOfRange);
+        void Join(ImportSourceType importSource, IDBReader dbReaderManager, IDBReader dbReaderManager2);
         void Stop();
 
         DataSource ParseModeType { get; }
