43cee642e6df19006ad0611d9a6f6e67d1411c34
David Smith <dsmith@datasync.com>
2010-03-23 08:55:23 +0000

    Calculate and store new memloc location. Update version numbers on all assemblies.

--- ParserCore/Properties/Settings.Designer.cs
+++ ParserCore/Properties/Settings.Designer.cs
@@ -56,7 +56,7 @@ namespace WaywardGamers.KParser.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("5767640")]
+        [global::System.Configuration.DefaultSettingValueAttribute("5772568")]
         public uint MemoryOffset {
             get {
                 return ((uint)(this["MemoryOffset"]));
