0d835826a7d7e177bcc8e48f2297f3a025af6131
David Smith <dsmith@datasync.com>
2010-04-11 14:02:28 +0000

    Add a new base plugin control class for a plugin that displays a graph (reference ZedGraph library) instead of text.

---
+++ ParserCore/Interface/BaseGraphPluginControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WaywardGamers.KParser.Plugin
+{
+    public partial class BaseGraphPluginControl : BasePluginControl
+    {
+        public BaseGraphPluginControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
