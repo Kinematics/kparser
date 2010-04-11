b4de37433d915d093b078e5b66e8df9d8d2a50d3
David Smith <dsmith@datasync.com>
2010-04-11 15:33:11 +0000

    Make the graph Protected so that derived classes can modify it.

--- ParserCore/Interface/BaseGraphPluginControl.cs
+++ ParserCore/Interface/BaseGraphPluginControl.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using WaywardGamers.KParser.Database;
 
 namespace WaywardGamers.KParser.Plugin
 {
@@ -14,6 +15,21 @@ namespace WaywardGamers.KParser.Plugin
         public BaseGraphPluginControl()
         {
             InitializeComponent();
+
+
+            IsActive = false;
+            MobXPHandler.Instance.CustomMobFilterChanged += this.CustomMobFilterChanged;
+
+            // Don't call this during the base constructor.  Let the plugins call it themselves.
+            // While it has the same effect, putting it in the constructor allows for
+            // an explicit reminder of what needs to be set.
+            //LoadLocalizedUI();
+            LoadResources();
+
+        }
+
+        protected void ResetGraph()
+        {
         }
     }
 }
