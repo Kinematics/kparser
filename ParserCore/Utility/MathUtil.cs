aef7988cf2d52a1199fd652115d86b5285f2cde3
David Smith <dsmith@datasync.com>
2010-07-02 20:42:41 +0000

    Add extension class for math utility functions.

---
+++ ParserCore/Utility/MathUtil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WaywardGamers.KParser.Utility
+{
+    public static class MathUtil
+    {
+        public static double GetHarmonicMean(this List<int> tsIndexes)
+        {
+            double denom = 0;
+
+            foreach (var index in tsIndexes)
+                denom += (double)1 / index;
+
+            return (tsIndexes.Count / denom);
+        }
+
+        public static TimeSpan GetHarmonicMean(this List<TimeSpan> tsIndexes)
+        {
+            double denom = 0;
+
+            foreach (var index in tsIndexes)
+            {
+                denom += 1 / index.TotalSeconds;
+            }
+
+            TimeSpan hMean = TimeSpan.FromSeconds(tsIndexes.Count / denom);
+
+            return hMean;
+        }
+    }
+}
