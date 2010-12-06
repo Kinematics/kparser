059413bb045c36e973b6598baa30aeb58cb441a6
David Smith <dsmith@datasync.com>
2010-12-06 23:21:52 +0000

    Update new default memloc.

--- ParserCore/Properties/Settings.Designer.cs
+++ ParserCore/Properties/Settings.Designer.cs
@@ -56,7 +56,7 @@ namespace WaywardGamers.KParser.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("5777752")]
+        [global::System.Configuration.DefaultSettingValueAttribute("5781832")]
         public uint MemoryOffset {
             get {
                 return ((uint)(this["MemoryOffset"]));
