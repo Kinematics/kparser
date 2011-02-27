880aa174726cee1ae3efdce83b12aecd9fa4a2bf
David Smith <dsmith@datasync.com>
2011-02-27 22:20:56 +0000

    Update version numbers and copyright info.

--- PluginUnparsed/Properties/AssemblyInfo.cs
+++ PluginUnparsed/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Wayward Gamers")]
 [assembly: AssemblyProduct("PluginUnparsed")]
-[assembly: AssemblyCopyright("Copyright © Wayward Gamers 2008-2010")]
+[assembly: AssemblyCopyright("Copyright © Wayward Gamers 2008-2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
