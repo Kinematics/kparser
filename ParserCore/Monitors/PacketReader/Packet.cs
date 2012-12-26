1f690dea7d01a53b3e5254bcd8def2a56fffc460
David Smith <dsmith@datasync.com>
2012-12-25 23:32:56 -0600

    Add class for handling reading bitstreamed packet data. Add class for constructing data structure out of bitstreamed packet.

---
+++ ParserCore/Monitors/PacketReader/Packet.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WaywardGamers.KParser.Monitoring.Packet
+{
+    public class Packet
+    {
+        RawPacket rawPacket;
+
+        #region Constructors
+        public Packet(byte[] packetBytes)
+        {
+            this.rawPacket = new RawPacket(packetBytes);
+            ReadRawPacket();
+        }
+
+        public Packet(string packetString, bool isBase64)
+        {
+            this.rawPacket = new RawPacket(packetString, isBase64);
+            ReadRawPacket();
+        }
+        #endregion
+
+        private void ReadRawPacket()
+        {
+            try
+            {
+                PacketID = rawPacket.ReadByte();
+                PacketLength = rawPacket.ReadByte();
+                PacketNum1 = rawPacket.ReadByte();
+                PacketNum2 = rawPacket.ReadByte();
+                PacketNum3 = rawPacket.ReadByte();
+                ActorID = rawPacket.ReadInt();
+                TargetCount = rawPacket.ReadInt(10);
+                Category = rawPacket.ReadInt(4);
+                Param = rawPacket.ReadInt(10);
+                AnimationCategory = rawPacket.ReadInt(6);
+                Animation = rawPacket.ReadInt(16);
+                Unknown1 = rawPacket.ReadInt(32);
+
+                Targets = new List<Target>(TargetCount);
+                for (int i = 0; i < TargetCount; i++)
+                {
+                    Targets.Add(new Target(rawPacket));
+                }
+
+                Unknown2 = rawPacket.ReadShort(16);
+
+                rawPacket.ReportStatus();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public byte PacketID { get; private set; }
+        public byte PacketLength { get; private set; }
+        public byte PacketNum1 { get; private set; }
+        public byte PacketNum2 { get; private set; }
+        public byte PacketNum3 { get; private set; }
+        public int ActorID { get; private set; }
+        public int TargetCount { get; private set; }
+
+        public int Category { get; private set; }
+        public int Param { get; private set; }
+        public int AnimationCategory { get; private set; }
+        public int Animation { get; private set; }
+        public int Unknown1 { get; private set; }
+        public short Unknown2 { get; private set; }
+
+        public List<Target> Targets { get; private set; }
+    }
+
+    public class Target
+    {
+        public Target(RawPacket rawPacket)
+        {
+            ReadTargetData(rawPacket);
+        }
+
+        private void ReadTargetData(RawPacket rawPacket)
+        {
+            TargetID = rawPacket.ReadInt();
+            ActionCount = rawPacket.ReadInt(4);
+
+            Actions = new List<Action>(ActionCount);
+            for (int i = 0; i < ActionCount; i++)
+            {
+                Actions.Add(new Action(rawPacket));
+            }
+        }
+
+        public int TargetID { get; private set; }
+        public int ActionCount { get; private set; }
+
+        public List<Action> Actions { get; private set; }
+    }
+
+
+    public class Action
+    {
+        public Action(RawPacket rawPacket)
+        {
+            ReadActionData(rawPacket);
+        }
+
+        private void ReadActionData(RawPacket rawPacket)
+        {
+            Unknown1 = rawPacket.ReadBool();
+
+            Reaction = rawPacket.ReadInt(5);
+            Animation = rawPacket.ReadInt(11);
+            Effect = rawPacket.ReadInt(4);
+            Stagger = rawPacket.ReadInt(5);
+            Param = rawPacket.ReadInt(17);
+            Message = rawPacket.ReadInt(10);
+
+            HasAdditionalEffect = rawPacket.ReadBool();
+            if (HasAdditionalEffect)
+            {
+                AdditionalEffect = new ExtraEffect(rawPacket);
+            }
+            else
+            {
+                AdditionalEffect = null;
+            }
+
+            HasSpikeEffect = rawPacket.ReadBool();
+            if (HasSpikeEffect)
+            {
+                SpikeEffect = new ExtraEffect(rawPacket);
+            }
+            else
+            {
+                SpikeEffect = null;
+            }
+
+            Unknown2 = rawPacket.ReadInt(32);
+
+        }
+
+        public int Reaction { get; private set; }
+        public int Animation { get; private set; }
+        public int Effect { get; private set; }
+        public int Stagger { get; private set; }
+        public int Param { get; private set; }
+        public int Message { get; private set; }
+
+        public bool HasAdditionalEffect { get; private set; }
+        public ExtraEffect AdditionalEffect { get; private set; }
+
+        public bool HasSpikeEffect { get; private set; }
+        public ExtraEffect SpikeEffect { get; private set; }
+
+        public bool Unknown1 { get; private set; }
+        public int Unknown2 { get; private set; }
+
+    }
+
+    public class ExtraEffect
+    {
+        public ExtraEffect(RawPacket rawPacket)
+        {
+            ReadEffectData(rawPacket);
+        }
+
+        private void ReadEffectData(RawPacket rawPacket)
+        {
+            Animation = rawPacket.ReadInt(6);
+            Effect = rawPacket.ReadInt(4);
+            Param = rawPacket.ReadInt(14);
+            Message = rawPacket.ReadInt(13);
+        }
+
+        public int Animation { get; private set; }
+        public int Effect { get; private set; }
+        public int Param { get; private set; }
+        public int Message { get; private set; }
+    }
+
+}
---
+++ ParserCore/Monitors/PacketReader/RawPacket.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace WaywardGamers.KParser.Monitoring.Packet
+{
+    public class RawPacket
+    {
+        byte[] packetBytes;
+
+        int bitPointer = 0;
+        int bytePointer = 0;
+
+        #region Constructors
+        public RawPacket(byte[] packetBytes)
+        {
+            this.packetBytes = packetBytes;
+        }
+
+        public RawPacket(string packetString, bool isBase64)
+        {
+            // If in base64, assume it's the full raw byte array data.
+            if (isBase64)
+            {
+                this.packetBytes = System.Convert.FromBase64String(packetString);
+            }
+            else
+            {
+                // If we're simply given an arbitrary string, it can be presented as something like:
+                // 28 18 79 10 2A D3 E6 05 00 01 44 18 DD 1A 0C 00 00 00 40 54
+                // So take two characters at a time, convert them to numeric values, and store in the array.
+
+                string byteString = string.Empty;
+                List<byte> byteList = new List<byte>(packetString.Length / 2);
+
+                foreach (char c in packetString)
+                {
+                    if (c != ' ')
+                    {
+                        byteString += c;
+
+                        if (byteString.Length == 2)
+                        {
+                            byteList.Add((byte)int.Parse(byteString, NumberStyles.AllowHexSpecifier));
+                            byteString = string.Empty;
+                        }
+                    }
+                }
+
+                this.packetBytes = byteList.ToArray();
+            }
+        }
+        #endregion
+
+        #region Private methods
+        private void IncrementBit()
+        {
+            bitPointer++;
+            if (bitPointer > 7)
+            {
+                bitPointer = 0;
+                bytePointer++;
+            }
+        }
+
+        internal void ReportStatus()
+        {
+            Trace.WriteLine(string.Format("Packet Length: {0}  Byte pointer: {1}  Bit pointer: {2}",
+                packetBytes.Length, bytePointer, bitPointer));
+        }
+        #endregion
+
+
+        #region Public methods
+
+        public byte ReadByte(int bits)
+        {
+            if (bits > 8)
+                throw new ArgumentOutOfRangeException("bits", bits, "Invalid number of bits requested");
+
+            byte value = 0;
+
+            // 0 or fewer requested bits, just return 0
+            if (bits < 1)
+                return value;
+
+            for (int bit = 0; bit < bits; bit++)
+            {
+                if (bytePointer >= packetBytes.Length)
+                    throw new IndexOutOfRangeException("Exceeded length of packet data.");
+
+                value |= (byte)(((packetBytes[bytePointer] >> bitPointer) & 1) << bit);
+
+                IncrementBit();
+            }
+
+            return value;
+        }
+
+        public short ReadShort(int bits)
+        {
+            if ((bits < 1) || (bits > 16))
+                throw new ArgumentOutOfRangeException("bits", bits, "Invalid number of bits requested");
+
+            byte[] bytes = new byte[2];
+
+            int bitsToRead = bits;
+            if (bitsToRead > 8)
+                bitsToRead = 8;
+            if (bitsToRead > 0)
+                bytes[0] = ReadByte(bitsToRead);
+
+            bitsToRead = bits - 8;
+            if (bitsToRead > 8)
+                bitsToRead = 8;
+            if (bitsToRead > 0)
+                bytes[1] = ReadByte(bitsToRead);
+
+            return BitConverter.ToInt16(bytes, 0);
+        }
+
+        public int ReadInt(int bits)
+        {
+            if ((bits < 1) || (bits > 32))
+                throw new ArgumentOutOfRangeException("bits", bits, "Invalid number of bits requested");
+
+            byte[] bytes = new byte[4];
+
+            int bitsToRead = bits;
+            if (bitsToRead > 8)
+                bitsToRead = 8;
+            if (bitsToRead > 0)
+                bytes[0] = ReadByte(bitsToRead);
+
+            bitsToRead = bits - 8;
+            if (bitsToRead > 8)
+                bitsToRead = 8;
+            if (bitsToRead > 0)
+                bytes[1] = ReadByte(bitsToRead);
+
+            bitsToRead = bits - 16;
+            if (bitsToRead > 8)
+                bitsToRead = 8;
+            if (bitsToRead > 0)
+                bytes[2] = ReadByte(bitsToRead);
+
+            bitsToRead = bits - 24;
+            if (bitsToRead > 8)
+                bitsToRead = 8;
+            if (bitsToRead > 0)
+                bytes[3] = ReadByte(bitsToRead);
+
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        /// <summary>
+        /// Quick check for a bool bit.
+        /// </summary>
+        /// <returns></returns>
+        public bool ReadBool()
+        {
+            return (ReadByte(1) == 1);
+        }
+
+        /// <summary>
+        /// Shortcut read for reading a full byte.
+        /// </summary>
+        /// <param name="value">Value read will be returned in this variable.</param>
+        public byte ReadByte()
+        {
+            return ReadByte(8);
+        }
+
+        /// <summary>
+        /// Shortcut read for reading a full int16.
+        /// </summary>
+        /// <param name="value">Value read will be returned in this variable.</param>
+        public short ReadShort()
+        {
+            return ReadShort(16);
+        }
+
+        /// <summary>
+        /// Shortcut read for reading a full int32.
+        /// </summary>
+        /// <param name="value">Value read will be returned in this variable.</param>
+        public int ReadInt()
+        {
+            return ReadInt(32);
+        }
+
+        /// <summary>
+        /// Reset pointers to the start of the packet.
+        /// </summary>
+        public void Reset()
+        {
+            bitPointer = 0;
+            bytePointer = 0;
+        }
+
+        #endregion
+    }
+}
